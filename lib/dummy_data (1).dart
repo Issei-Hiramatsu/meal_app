b02a6cec21cd588495edabaa4eaef1075a35b591
Issei-Hiramatsu <107228316+Issei-Hiramatsu@users.noreply.github.com>
2022-09-02 11:49:27 +0900

    Update dummy_data (1).dart

--- lib/dummy_data (1).dart
+++ lib/dummy_data (1).dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import './models/category.dart';
 
-const  DUMMY_CATEGORIES = ConstraintsTransformBox[
+final DUMMY_CATEGORIES = [
   Category(
     id: 'c1',
     title: 'Italian',
