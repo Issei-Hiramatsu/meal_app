c21b4828fcf92d6899fdfe82f92feac664c6b0a6
issei <issei.hiramatsu@gmail.com>
2022-09-02 21:17:10 +0900

    route機能

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import './categories_screen.dart';
+import './category_meals_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,26 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(child: Text('Navigation Time')),
+      home: CategoriesScreen(),
+      routes: {'/categories-meals': (ctx) => CategoryMealsScreen()},
     );
   }
 }
