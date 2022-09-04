5f5fa5f1a8d4f49ace562bd7f79ace7640646711
issei <issei.hiramatsu@gmail.com>
2022-09-04 12:08:11 +0900

    画面遷移の追加　ページが無限に増えないように対処

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:meal_app/screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData icon) {
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
       leading: Icon(
         icon,
@@ -17,7 +18,7 @@ class MainDrawer extends StatelessWidget {
           fontWeight: FontWeight.bold,
         ),
       ),
-      onTap: () {},
+      onTap: () => tapHandler(),
     );
   }
 
@@ -43,8 +44,12 @@ class MainDrawer extends StatelessWidget {
         SizedBox(
           height: 20,
         ),
-        buildListTile('Meals', Icons.restaurant),
-        buildListTile('Filters', Icons.settings),
+        buildListTile('Meals', Icons.restaurant, () {
+          Navigator.of(context).pushReplacementNamed('/');
+        }),
+        buildListTile('Filters', Icons.settings, () {
+          Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+        }),
       ],
     ));
   }
